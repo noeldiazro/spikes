1ee4881ea6ec087f10f494388663842a54d1b509
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-04-08 18:34:08 +0200

    Tests moved to subpackage

--- junit/src/MovieList.java
+++ junit/src/MovieList.java
@@ -1,5 +1,11 @@
-class MovieList {
-    int size() {
+package movies;
+
+public class MovieList {
+    public int size() {
 	return 0;
+    }
+
+    public boolean isEmpty() {
+	return true;
     }
 }
--- junit/src/MovieListTest.java
+++ junit/src/MovieListTest.java
@@ -1,8 +1,21 @@
+package movies.tests;
+
 import junit.framework.TestCase;
+import movies.MovieList;
 
 public class MovieListTest extends TestCase {
+    private MovieList emptyList;
+    
+    @Override protected void setUp() throws Exception {
+	super.setUp();
+	emptyList = new MovieList();
+    }
+    
     public void testEmptyList_SizeIsZero() {
-	MovieList emptyList = new MovieList();
 	assertEquals(0, emptyList.size());
+    }
+
+    public void testEmptyList_IsEmpty() {
+	assertTrue(emptyList.isEmpty());
     }
 }
