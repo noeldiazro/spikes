3b3fe370bb346e2870aedeb1cc03a26f5433e026
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-04-09 10:10:38 +0200

    Package restructured

---
+++ junit/src/movies/tests/EmptyListTest.java
@@ -0,0 +1,31 @@
+package movies.tests;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import junit.framework.TestCase;
+
+public class EmptyListTest extends TestCase {
+    private List emptyList;
+    
+    @Override protected void setUp() throws Exception {
+	super.setUp();
+	emptyList = new ArrayList();
+    }
+    
+    public void testSize() {
+	assertEquals("Size of an empty list should be zero.",
+		     0,
+		     emptyList.size());
+    }
+
+    public void testIsEmpty() {
+	assertTrue("Empty list should report empty.", emptyList.isEmpty());
+    }
+
+    public void testIterator() {
+	Iterator emptyListIterator = emptyList.iterator();
+	assertFalse("Iterator from empty list should be empty.",
+		    emptyListIterator.hasNext());
+    }
+}
---
+++ junit/src/movies/tests/OneItemListTest.java
@@ -0,0 +1,24 @@
+package movies.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+import junit.framework.TestCase;
+
+public class OneItemListTest extends TestCase {
+    private List oneItemList;
+
+    @Override protected void setUp() {
+	oneItemList = new ArrayList();
+	oneItemList.add("One");
+    }
+    
+    public void testSize() {
+	assertEquals("Size of a one item list should be one.",
+		     1,
+		     oneItemList.size());
+    }
+
+    public void testIsEmpty() {
+	assertFalse("One item list should report non-empty.", oneItemList.isEmpty());
+    }
+}
